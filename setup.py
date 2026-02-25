5086613b4ebe51c7eb023406e630b966071faa3b
kodonnell <kodonnell@users.noreply.github.com>
2026-02-25 13:53:49 +1300

    try that

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-import toml
 import sys
 
 import setuptools_scm  # noqa: F401 - to avoid version = 0.0.0 errors if built without setuptools_scm installed
@@ -30,18 +29,15 @@ def no_cythonize(extensions, **_ignore):
         extension.sources[:] = sources
     return extensions
 
-define_macros=[]
-if (
-    sys.version_info > (3, 13, 0)
-    and hasattr(sys, "_is_gil_enabled")
-    and not sys._is_gil_enabled()
-):
+
+define_macros = []
+if sys.version_info > (3, 13, 0) and hasattr(sys, "_is_gil_enabled") and not sys._is_gil_enabled():
     print("build nogil")
     define_macros.append(
         ("Py_GIL_DISABLED", "1"),
     )
 
-extensions = [Extension( "wkp.encode", sources=["src/wkp/encode.pyx"],  define_macros=define_macros)]
+extensions = [Extension("wkp.encode", sources=["src/wkp/encode.pyx"], define_macros=define_macros)]
 
 
 if USE_CYTHON:
@@ -59,17 +55,4 @@ else:
     extensions = no_cythonize(extensions)
 
 
-# Read dependencies from pyproject.toml
-with open("pyproject.toml") as f:
-    pyproject = toml.load(f)
-install_requires = pyproject["project"].get("dependencies", [])
-dev_requires = pyproject["project"]["optional-dependencies"]["dev"]
-
-setup(
-    ext_modules=extensions,
-    install_requires=install_requires,
-    extras_require={
-        "dev": dev_requires,
-    },
-    include_dirs=[],
-)
+setup(ext_modules=extensions)
